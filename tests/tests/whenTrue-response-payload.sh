e4c397dfd3362a0b829abe2de5f0007199874ab0
m.tarrade@sap.com <m.tarrade@sap.com>
2024-04-17 11:49:22 +0200

    add tests for whenTrue payload

---
+++ tests/tests/whenTrue-response-payload.sh
@@ -0,0 +1,61 @@
+# TEMPLATE FOR YOUR OWN TESTS
+
+# Configure decoys
+config='
+{
+  "filters": [
+    {
+      "decoy": {
+        "key": "x-cloud-active-defense",
+        "separator": "=",
+        "value": "ACTIVE"
+      },
+      "inject": {
+        "store": {
+          "inResponse": ".*",
+          "as": "header",
+          "whenTrue": [
+            {
+              "value": "Welcome",
+              "in": "payload"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}
+'
+
+# connect to configmanager, update /data/cad-default.json
+echo "$config" | docker exec -i configmanager sh -c 'cat > /data/cad-default.json'
+# wait a few seconds for the proxy to read the new config
+sleep 5
+
+
+# Start timing
+start_time=$(date +%s.%N)
+
+# Temporary file for curl output
+tempfile=$(bash ./uuidgen.sh)
+
+# Do relevant action(s)
+curl -v http://localhost:8000/ >$tempfile 2>&1
+
+# Check INJECTION (in $tempfile)
+status=$(grep "< x-cloud-active-defense: ACTIVE" $tempfile)
+
+# Output result & time
+if [ -z "$status" ]; then
+  echo -e "\033[0;31mFAIL\033[0m"
+else
+  echo -e "\033[0;32mPASS\033[0m"
+fi
+
+check_1_time=$(date +%s.%N)
+execution_time=$(echo "$check_1_time $start_time" | awk '{print $1 - $2}')
+echo "Execution time: $execution_time seconds"
+
+# Cleanup
+rm $tempfile
+
