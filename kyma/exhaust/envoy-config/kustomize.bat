2ab14ece1c68cc8811ae6bd6acb8d59309cb6ced
mtarrade.sap@gmail.com <mtarrade.sap@gmail.com>
2024-09-04 14:50:40 +0200

    ✏️update demo namespace typo

--- kyma/exhaust/envoy-config/kustomize.bat
+++ kyma/exhaust/envoy-config/kustomize.bat
@@ -1,6 +1,6 @@
 @echo off
 set dir=%~dp0
 
-kubectl get envoyfilter test-myapp-cloudactivedefensefilter -n cad -o yaml > %dir%/resources.yaml
+kubectl get envoyfilter test-myapp-cloudactivedefensefilter -n demo-ns -o yaml > %dir%/resources.yaml
 kubectl apply -k %dir%
 del "%dir%/resources.yaml"
--- kyma/exhaust/envoy-config/kustomize.sh
+++ kyma/exhaust/envoy-config/kustomize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-kubectl get envoyfilter test-myapp-cloudactivedefensefilter -n cad -o yaml > "$dir/resources.yaml"
+kubectl get envoyfilter test-myapp-cloudactivedefensefilter -n demo-ns -o yaml > "$dir/resources.yaml"
 kubectl apply -k "$dir"
 rm -f "$dir/resources.yaml"
