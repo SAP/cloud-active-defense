e300e93667bae1e96f72a4a41fab09e4e0178531
mtarrade.sap@gmail.com <mtarrade.sap@gmail.com>
2025-02-06 14:43:05 +0100

    🗃️update db schema

--- controlpanel/db/schema.sql
+++ controlpanel/db/schema.sql
@@ -1,19 +1,8 @@
 CREATE DATABASE cad;
 
-CREATE TABLE cad.decoys (
-    id UUID PRIMARY KEY NOT NULL,
-    state VARCHAR(255) NOT NULL,
-    pa_id UUID NOT NULL,
-    deployed BOOLEAN NOT NULL,
-    decoy JSON NOT NULL,
-    createdAt TIMESTAMPTZ NOT NULL,
-    updatedAt TIMESTAMPTZ NOT NULL
-    CONSTRAINT fk_pa_id
-        FOREIGN KEY (pa_id)
-        REFERENCES protectedApps(id)
-);
+\c cad;
 
-CREATE TABLE cad.protectedApps (
+CREATE TABLE "protectedApps" (
     id UUID PRIMARY KEY NOT NULL,
     namespace VARCHAR(255) NOT NULL,
     application VARCHAR(255) NOT NULL,
@@ -21,7 +10,20 @@ CREATE TABLE cad.protectedApps (
     updatedAt TIMESTAMPTZ NOT NULL
 );
 
-CREATE TABLE cad.config (
+CREATE TABLE decoys (
+    id UUID PRIMARY KEY NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    pa_id UUID NOT NULL,
+    deployed BOOLEAN NOT NULL,
+    decoy JSON NOT NULL,
+    createdAt TIMESTAMPTZ NOT NULL,
+    updatedAt TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_pa_id
+        FOREIGN KEY (pa_id)
+        REFERENCES "protectedApps"(id)
+);
+
+CREATE TABLE config (
     id UUID PRIMARY KEY NOT NULL,
     pa_id UUID NOT NULL,
     deployed BOOLEAN NOT NULL,
@@ -30,12 +32,16 @@ CREATE TABLE cad.config (
     updatedAt TIMESTAMPTZ NOT NULL,
     CONSTRAINT fk_pa_id
         FOREIGN KEY (pa_id)
-        REFERENCES protectedApps(id)
+        REFERENCES "protectedApps"(id)
 );
 
-CREATE TABLE cad.logs (
+CREATE TABLE logs (
     id UUID PRIMARY KEY NOT NULL,
+    pa_id UUID NOT NULL,
     date INTEGER NOT NULL,
     type VARCHAR(255) NOT NULL,
     content JSONB NOT NULL,
+    CONSTRAINT fk_pa_id
+        FOREIGN KEY (pa_id)
+        REFERENCES "protectedApps"(id)
 )
