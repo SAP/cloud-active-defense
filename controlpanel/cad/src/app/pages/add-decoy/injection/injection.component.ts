33ab440262c658ae11de244f5ba8c038b76fb1dd
mtarrade.sap@gmail.com <mtarrade.sap@gmail.com>
2025-05-15 10:48:24 +0200

    🩹fix dynamic key/value on add decoy page

--- controlpanel/cad/src/app/pages/add-decoy/injection/injection.component.ts
+++ controlpanel/cad/src/app/pages/add-decoy/injection/injection.component.ts
@@ -286,9 +286,9 @@ export class InjectionComponent implements OnInit, ValidateDecoyFormDeactivate, 
       request: decoyData.inject?.store.inResponse || decoyData.inject?.store.inRequest ? (decoyData.inject?.store.inResponse ? 'inResponse' : 'inRequest') : 'inResponse',      
       verb: decoyData.inject?.store.withVerb || '',
       as: decoyData.inject?.store.as || 'header',
-      key: decoyData.decoy.key || '',
+      key: decoyData.decoy.key || decoyData.decoy.dynamicKey || '',
       separator: decoyData.decoy.separator || '',
-      value: decoyData.decoy.value || '',
+      value: decoyData.decoy.value || decoyData.decoy.dynamicValue || '',
       string: decoyData.decoy.string || '',
       atMethod: decoyData.inject?.store.at?.method || '',
       atProperty: decoyData.inject?.store.at?.property || ''
@@ -299,6 +299,8 @@ export class InjectionComponent implements OnInit, ValidateDecoyFormDeactivate, 
     if (decoyData.inject?.whenTrue){
       this.whenArray = [...this.whenArray, ...decoyData.inject.whenTrue.map(item => ({ ...item, type: true }))];
     }
+    if (decoyData.decoy.dynamicKey) this.onRegexChange('key');
+    if (decoyData.decoy.dynamicValue) this.onRegexChange('value');
   }
 
   skipInjection() {
