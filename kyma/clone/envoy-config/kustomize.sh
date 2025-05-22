529220473377f47c7fb9d0bb7e00079e78bd9266
mtarrade.sap@gmail.com <mtarrade.sap@gmail.com>
2025-05-22 10:18:58 +0200

    🔥remove old clone/exhaust deployment

--- kyma/clone/envoy-config/kustomize.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-kubectl get envoyfilter test-myapp-cloudactivedefensefilter -n demo-ns -o yaml > "$dir/resources.yaml"
-kubectl apply -k "$dir"
-rm -f "$dir/resources.yaml"
