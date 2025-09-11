4ef043e0c1af591bc364850124f4d1a16986c11a
mtarrade.sap@gmail.com <mtarrade.sap@gmail.com>
2025-09-11 09:38:57 +0200

    📝update sql schema reference

--- controlpanel/db/schema.sql
+++ controlpanel/db/schema.sql
@@ -6,8 +6,12 @@ CREATE TABLE "protectedApps" (
     id UUID PRIMARY KEY NOT NULL,
     namespace VARCHAR(255) NOT NULL,
     application VARCHAR(255) NOT NULL,
+    cu_id UUID NOT NULL,
     createdAt TIMESTAMPTZ NOT NULL,
     updatedAt TIMESTAMPTZ NOT NULL
+    CONSTRAINT fk_cu_id
+        FOREIGN KEY (cu_id)
+        REFERENCES customers(id)
 );
 
 CREATE TABLE decoys (
@@ -22,7 +26,7 @@ CREATE TABLE decoys (
         REFERENCES "protectedApps"(id)
 );
 
-CREATE TABLE config (
+CREATE TABLE configs (
     id UUID PRIMARY KEY NOT NULL,
     pa_id UUID NOT NULL,
     deployed BOOLEAN NOT NULL,
@@ -44,3 +48,34 @@ CREATE TABLE logs (
         FOREIGN KEY (pa_id)
         REFERENCES "protectedApps"(id)
 )
+
+CREATE TABLE "apiKeys" (
+    id UUID PRIMARY KEY NOT NULL,
+    key VARCHAR(255) NOT NULL,
+    permissions VARCHAR(255) NOT NULL,
+    pa_id UUID,
+    expirationDate TIMESTAMPTZ NOT NULL,
+    createdAt TIMESTAMPTZ NOT NULL,
+    updatedAt TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_pa_id
+        FOREIGN KEY (pa_id)
+        REFERENCES "protectedApps"(id)
+);
+
+CREATE TABLE blocklists (
+    id UUID PRIMARY KEY NOT NULL,
+    pa_id UUID NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    date FLOAT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    content JSON NOT NULL
+    createdAt TIMESTAMPTZ NOT NULL,
+    updatedAt TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE customers (
+    id UUID PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    kubeconfig TEXT,
+    createdAt TIMESTAMPTZ NOT NULL,
+    updatedAt TIMESTAMPTZ NOT NULL
+);
